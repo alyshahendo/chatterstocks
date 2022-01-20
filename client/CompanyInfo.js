b946fe9e4b0cb9ca419714f672277f8cc2dea867
Alysha Gilliard <alyshargilliard@gmail.com>
2022-01-20 13:25:51 -0500

    Create companyInfo elements

--- client/CompanyInfo.js
+++ client/CompanyInfo.js
@@ -1,1 +1,10 @@
 // contains company name, price, ticker and company rating
+function CompanyInfo() {
+  return React.createElement(
+    "div",
+    { id: "company-info" },
+    React.createElement("div", { id: "company-name" }),
+    React.createElement("div", { id: "company-ticker" }),
+    React.createElement("div", { id: "company-price" })
+  );
+}
--- client/src/CompanyInfo.jsx
+++ client/src/CompanyInfo.jsx
@@ -1,1 +1,10 @@
 // contains company name, price, ticker and company rating
+function CompanyInfo () {
+  return (
+    <div id="company-info">
+      <div id="company-name"></div>
+      <div id="company-ticker"></div>
+      <div id="company-price"></div>
+    </div>
+  )
+}
