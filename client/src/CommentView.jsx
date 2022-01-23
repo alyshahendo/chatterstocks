564d0e408a04f2126c9dd2677cfcb75c854fbc5b
Alysha Gilliard <alyshargilliard@gmail.com>
2022-01-23 17:48:58 -0500

    Change CommentView to stateless component

--- client/src/CommentView.jsx
+++ client/src/CommentView.jsx
@@ -3,20 +3,15 @@ import React from 'react';
 import AddComment from './AddComment';
 import Comment from './Comment';
 
-class CommentView extends React.Component {
-  constructor(props) {
-    super(props)
-  }
 
-  render() {
-    return (
-      <div id="comment-view">
-        {this.props.comments.map((comment) => {
-          return <Comment key ={comment._id} comment={comment}/>
-        })}
-      </div>
-    )
-  }
+function CommentView (props) {
+  return (
+    <div id="comment-view">
+      {props.comments.map((comment) => {
+        return <Comment key ={comment._id} comment={comment}/>
+      })}
+    </div>
+  )
 }
 
 export default CommentView;
